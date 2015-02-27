552521a86f076381a27118d262f99fd035394ccb
Codrut Constantin Gusoi <codrut.gusoi@zitec.ro>
2015-02-27 18:20:10 +0200

    Some more refactoring

--- lib/redmine_target_version_status_filter/patches/issue_query_patch.rb
+++ lib/redmine_target_version_status_filter/patches/issue_query_patch.rb
@@ -8,7 +8,6 @@ module RedmineTargetVersionStatusFilter
     module IssueQueryPatch
       def self.included(base)
         base.send(:include, InstanceMethods)
-
         base.class_eval do
           unloadable
           alias_method_chain :statement, :target_version_status
@@ -18,62 +17,61 @@ module RedmineTargetVersionStatusFilter
       end
 
       module InstanceMethods
-
         def statement_with_target_version_status
-            filter  = filters.delete 'target_version_status'
-            clauses = statement_without_target_version_status || ''
-            if filter
-              filters.merge!( 'target_version_status' => filter )
+          filter  = filters.delete 'target_version_status'
+          clauses = statement_without_target_version_status || ''
+          if filter
+            filters.merge!( 'target_version_status' => filter )
 
-              op = operator_for('target_version_status')
+            op = operator_for('target_version_status')
 
-              project_versions = Version.where(:project_id => project_id)
-              apply_filter = true
-              case op
-              when '=', '!'
-                version_status = value_for('target_version_status')
-                if version_status == 'current'
-                  ids_list = [0]
-                  current_date = Date.today
-                  version_id = project_versions.where('effective_date >= ?', current_date).order(:effective_date).select(:id).first
-                  version = Version.where(id: version_id).first
-                  version.custom_field_values.each do |custom_field|
-                    if custom_field.custom_field.name == 'Start Date' && Date.parse(custom_field.value) <= current_date
-                      ids_list << version.id
-                      break
-                    end
+            project_versions = Version.where project_id: project_id
+            apply_filter = true
+            case op
+            when '=', '!'
+              version_status = value_for('target_version_status')
+              if version_status == 'current'
+                ids_list = [0]
+                current_date = Date.today
+                version_id = project_versions.where('effective_date >= ?', current_date).order(:effective_date).select(:id).first
+                version = Version.where(id: version_id).first
+                version.custom_field_values.each do |custom_field|
+                  if custom_field.custom_field.name == 'Start Date' && Date.parse(custom_field.value) <= current_date
+                    ids_list << version.id
+                    break
                   end
-                else
-                  ids_list = project_versions.where(:status => version_status.clone).pluck(:id).push(0)
                 end
-              when '!*'
-                ids_list = []
-                apply_filter = false
-                clauses << ' AND ' unless clauses.empty?
-                clauses << "( #{Issue.table_name}.fixed_version_id IS NULL ) "
               else
-                ids_list = project_versions.pluck(:id).push(0)
+                ids_list = project_versions.where(status: version_status.clone).pluck(:id).push(0)
               end
-              ids_list << 0
-              if apply_filter
-                compare   = op.eql?('!') ? 'NOT IN' : 'IN'
-                ids_list = ids_list.join(', ')
-
-                clauses << ' AND ' unless clauses.empty?
-                clauses << "( #{Issue.table_name}.fixed_version_id #{compare} (#{ids_list}) ) "
-              end
+            when '!*'
+              ids_list = []
+              apply_filter = false
+              clauses << ' AND ' unless clauses.empty?
+              clauses << "( #{Issue.table_name}.fixed_version_id IS NULL ) "
+            else
+              ids_list = project_versions.pluck(:id).push(0)
             end
-            clauses
+            ids_list << 0
+            if apply_filter
+              compare   = op.eql?('!') ? 'NOT IN' : 'IN'
+              ids_list = ids_list.join(', ')
+
+              clauses << ' AND ' unless clauses.empty?
+              clauses << "( #{Issue.table_name}.fixed_version_id #{compare} (#{ids_list}) ) "
+            end
+          end
+          clauses
         end
 
         def available_filters_with_target_version_status
           unless @available_filters
             available_filters_without_target_version_status.merge!({
               'target_version_status' => {
-                :name   => l(:target_version_status),
-                :type   => :list_optional,
-                :order  => 7,
-                :values => Version::VERSION_STATUSES.map{ |t| [t, t] } << ['current', 'current']
+                name: l(:target_version_status),
+                type: :list_optional,
+                order: 7,
+                values: Version::VERSION_STATUSES.map{ |t| [t, t] } << ['current', 'current']
               }
             })
           end
@@ -85,6 +83,6 @@ module RedmineTargetVersionStatusFilter
 end
 
 base = ActiveSupport::Dependencies::search_for_file('issue_query') ? IssueQuery : Query
-unless base.included_modules.include?(RedmineTargetVersionStatusFilter::Patches::IssueQueryPatch)
-  base.send(:include, RedmineTargetVersionStatusFilter::Patches::IssueQueryPatch)
+unless base.included_modules.include? RedmineTargetVersionStatusFilter::Patches::IssueQueryPatch
+  base.send :include, RedmineTargetVersionStatusFilter::Patches::IssueQueryPatch
 end
